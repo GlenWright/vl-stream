769877c919491d6bd31205f969942bc462312f94
Chris Franklin <chris@cwfranklin.com>
2019-10-01 22:01:34 +0100

    Removing extended functionality

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const streamio = io.of('/stream')
 const pug = require('pug')
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../public'))
 app.set('view engine', 'pug')
